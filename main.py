6ddea34ec31a94da528172cac86fb931505e609d
Victor Ponce <vicmanuelr@gmail.com>
2022-09-24 10:17:42 -0600

    Initial setup of GUI
    - window created and title
    - added canvas with logo image

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window related
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# Canvas Related
+lock_img = PhotoImage(file="logo.png")
+canvas = Canvas(width=202, height=202)
+canvas.create_image(100, 100, image=lock_img)
+canvas.pack()
+
+window.mainloop()
