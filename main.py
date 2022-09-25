b60b2c137aa914a59710ced26b09dcb45621fa82
Victor Ponce <vicmanuelr@gmail.com>
2022-09-25 13:38:42 -0600

    From Project: git@github.com:vicmanuelr/DAY5-PyPassword-Generator.git
    - created gen password function and added function to button

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random as rn
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# From Password Generator Project: git@github.com:vicmanuelr/DAY5-PyPassword-Generator.git
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+NR_LETTERS = rn.randint(8, 10)
+NR_SYMBOLS = rn.randint(2, 4)
+NR_NUMBERS = rn.randint(2, 4)
+
+
+def gen_password():
+    password_entry.delete(0, END)
+    # get random letters from list letters
+    password_constructor = [rn.choice(letters) for _ in range(0, NR_LETTERS)]
+    # get random symbols from list symbols
+    password_constructor += [rn.choice(symbols) for _ in range(0, NR_SYMBOLS)]
+    # get random numbers from list of numbers
+    password_constructor += [rn.choice(numbers) for _ in range(0, NR_NUMBERS)]
+    # shuffle or randomize password characters
+    rn.shuffle(password_constructor)
+    # join all passwords characters to deliver to user
+    final_password = "".join(password_constructor)
+    password_entry.insert(0, f"{final_password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -53,7 +79,7 @@ password_entry = Entry(width=28)
 password_entry.grid(column=1, row=3)
 
 # Buttons
-gen_pass = Button(text="Generate PW", justify="left", width=9)
+gen_pass = Button(text="Generate PW", justify="left", width=9, command=gen_password)
 gen_pass.grid(column=2, row=3)
 add_entry = Button(text="Add", width=38, command=save)
 add_entry.grid(column=1, columnspan=2, row=4)
